f6c82487e8d33c608384bd611a08e25eb8c50773
Vitor Pamplona <vitor@vitorpamplona.com>
2021-10-19 12:32:00 -0400

    Adding Australia, Philippines, SriLanka and fixing DCC's production QRs

--- test/divoc.spec.js
+++ test/divoc.spec.js
@@ -1,78 +1,83 @@
-const {verifyQRCode} = require('../lib/index');
-const expect = require('chai').expect; 
+const { verifyQRCode } = require('../lib/index')
+const expect = require('chai').expect
 
-const base32url = require('base32url');
+const base32url = require('base32url')
 
 const PRODUCTION_PAYLOAD = {
-  "@context":["https://www.w3.org/2018/credentials/v1","https://cowin.gov.in/credentials/vaccination/v1"],
-  "type":["VerifiableCredential","ProofOfVaccinationCredential"],
-  "credentialSubject":{
-    "type":"Person",
-    "id":"did:Passport:Dummy256",
-    "refId":"39791185041847",
-    "name":"Third March User One",
-    "gender":"Male",
-    "age":"65",
-    "nationality":"Indian",
-    "address":{
-      "streetAddress":"",
-      "streetAddress2":"",
-      "district":"Chamba",
-      "city":"",
-      "addressRegion":"Himachal",
-      "addressCountry":"IN",
-      "postalCode":176207
-    }
-  },
-  "issuer":"https://cowin.gov.in/",
-  "issuanceDate":"2021-03-03T04:28:46.012Z",
-  "evidence":[{
-    "id":"https://cowin.gov.in/vaccine/92047670169",
-    "feedbackUrl":"https://cowin.gov.in/?92047670169",
-    "infoUrl":"https://cowin.gov.in/?92047670169",
-    "certificateId":"92047670169",
-    "type":["Vaccination"],
-    "batch":"Dummy-TGN-Chamba",
-    "vaccine":"COVISHIELD",
-    "manufacturer":"Serum Institute of India",
-    "date":"2021-03-03T04:28:43.134Z",
-    "effectiveStart":"2021-03-03",
-    "effectiveUntil":"2021-03-03",
-    "dose":1,
-    "totalDoses":2,
-    "verifier":{
-      "name":"Dummy Vaccinator"
+  '@context': ['https://www.w3.org/2018/credentials/v1', 'https://divoc.lgcc.gov.lk/credentials/vaccination/v1'],
+  type: ['VerifiableCredential', 'ProofOfVaccinationCredential'],
+  credentialSubject: {
+    type: 'Person',
+    id: '962433308V',
+    refId: '962433308v',
+    name: 'GAVIN MATTHEW DE SILVA',
+    gender: 'Male',
+    age: '24',
+    nationality: 'Sri Lankan',
+    address: {
+      streetAddress: '209/2 Batalanda Road, Makola South, Makola',
+      streetAddress2: '',
+      district: ' ',
+      city: '',
+      addressRegion: 'state',
+      addressCountry: 'IN',
+      postalCode: 'pincode',
     },
-    "facility":{
-      "name":"Himachal Site Name 176207",
-      "address":{
-        "streetAddress":"Address Of Site 176207",
-        "streetAddress2":"",
-        "district":"Chamba",
-        "city":"",
-        "addressRegion":"Himachal",
-        "addressCountry":"IN",
-        "postalCode":""}
-      }
-    }
+  },
+  issuer: 'https://divoc.lgcc.gov.lk/',
+  issuanceDate: '2021-09-24T16:25:43.497Z',
+  evidence: [
+    {
+      id: 'https://divoc.lgcc.gov.lk/vaccine/819152878',
+      feedbackUrl: 'https://divoc.lgcc.gov.lk/?819152878',
+      infoUrl: 'https://divoc.lgcc.gov.lk/?819152878',
+      certificateId: '819152878',
+      type: ['Vaccination'],
+      batch: 'SINOPHARM (VERO CELL) - 202106B1269',
+      vaccine: 'SINOPHARM (VERO CELL)',
+      manufacturer: 'SINOPHARM (VERO CELL)',
+      date: '2021-07-15T00:00:00.000Z',
+      effectiveStart: '2020-12-15',
+      effectiveUntil: '2021-01-15',
+      dose: 1,
+      totalDoses: 2,
+      verifier: { name: 'Unknown' },
+      facility: {
+        name: 'ABC Medical Center',
+        address: {
+          streetAddress: 'addressLine1',
+          streetAddress2: 'addressLine2',
+          district: 'district',
+          city: '',
+          addressRegion: 'state',
+          addressCountry: 'IN',
+          postalCode: 'pincode',
+        },
+      },
+    },
   ],
-  "nonTransferable":"true",
-  "proof":{
-    "type":"RsaSignature2018",
-    "created":"2021-03-03T04:28:46Z",
-    "verificationMethod":"did:india",
-    "proofPurpose":"assertionMethod",
-    "jws":"eyJhbGciOiJQUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..nE-3iYDcKok-CmTm3pbpyZQheqpVDor_f0YQt4ukWDGyZuqtZuu8NROtJjOZ-LNAavN-JZtCAdunofe19-mcC2HC20W_yxzGfB1Idft15CruJWOuvkkKXX0UoezZhsk_Cd-HmeHeCCUgoyLiDtpSqzXd9WqiHG_XN39PUMvIHnZAPo54sodtzAEX88L1GSSd37JBrOlKoeMMJaC0JDIEDSFV-SaVk_vH5e06Vq86WrkMj5oj4pqmgnql7W_qt3NAgNqQQgUUNhsMOHwHXKBr5j80yjp1LRMidB81u0SQJvbxQVxLnphUbPcTI4h6nLJeZJjOiecOLESgIELpC2_SDg"
-  }
-};
+  nonTransferable: 'true',
+  proof: {
+    type: 'RsaSignature2018',
+    created: '2021-09-24T16:25:43Z',
+    verificationMethod: 'did:srilanka:moh',
+    proofPurpose: 'assertionMethod',
+    jws: 'eyJhbGciOiJQUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..BADKnZGgtmhgSmU4gaNty2GXIV1NyxPFzFqK2t-QSBTXCaXxM991Mht63wyGvQBAZGg6xSqEF1RRffFYtSfqw8nYKGh-OtfG-CNVU2OnUBnzH0k0J9VRg8gsOiTXLJ4B7PAov41jZO1_nnzbXfNMxxkfgMQ-SKovXNOXbSKo-DxoVLfabYmrWWyf0_ioV4CddUjRuOdQsiSJ9ZDuXOIKXl8RQo5tKXj0GqZUxrvV2tu_nbOsHp0IiEle2Ur48iCEYiXQU-RC6poIvEcQBkQONDDefPbw-oGr8wKuySkfHvvpq3-FNGa44Kty5H-ZV8FAhaEk265Qy3Ff9MpLJI_lMw',
+  },
+}
 
-// payload is actually binary, but we are representing here in B64
-const PRODUCTION_PAYLOAD_B32URL = "KBFQGBAKAAAAACAAB7BKDQ4DKIFMHBGDSLB2FQ5EAMAABQ4ZA4AAAEAAAAAGGZLSORUWM2LDMF2GKLTKONXW5Q4NKROXHQ5KHAGMHPJLBTBY6CZEYOQ4FG6CS7BLWNGDWRBCQJGDSDB3AVOCSYO4HBRYJZRCA5WCWAOSRQ5NYO2MFP6CV7B2HQ4QII54HLODRTB25Q43YOHMHMCAYKSCGWKHHJJMHPTVYKK4HPYTKIRMHEGCWNMMHJK3P7BK3Q5SYKQRAMLPYKKUVQVHYOJ4FKLYFILSSC2KYKDMFJRXJLBJAIIPCHBIDQ4BYKPMFF6CR3B3UKWDX7BYOFIKYOUQTQUTMJAMFD2FJQ7MHIYAYKCMFGAAYKASSUJRP7BYWIDRYKHFDQ5WYOJAYMODVRR4FMGDTER4HMZDI3B2KHJRJJ64HB6CT5O4FI3PAFU4FEWDVMRW5QVSYOMSFQUYYOLMHPWDXLBJCW3GIDBIYU6CULBJEYKPLE6MHLGCWVDMFAGDWPBJQMWDSHB2UJCROQ3MFKRVAVQMHCGCW4ZEZQVZLFX4HKT2YKR4FKSVYO2ENQVFYKXMFHAEIRM4HBSJYKEMFGOCS4NQEBWDQPBZZQUUEPBJM4YIKLBIQAARB4Y4FBIZYKBH2ZQDIELFJQVLL3BJFQVEDTBYAHWCRPBLGMS3YOCMHAZAYKVQ46A6IPBJYZ6DUVZ4HAIQCLB22D6DSMVMFLZQYKP4HDGDQ3BZLQ5OMHB2TQ4BYKFMHOZKN7BIMIGDTIAGLQUHYOX4HL6DTTBL5Q5QYKEAFQU4OZCMFGT3HABDBQV4YO2MHOWDUI3GSQQEYK54FFDHFNHUZQVZAB54FE32FEL4FPK6GPBLJQ52YONVXQ42KDBY5QUTBPB2C3YFYKIHKXOCQIAMFAOCVABUJQ4WBJBTHQ5UYKBFNQUWYK74FCKWNEM4FDKWYKSVNQ4UORR4FKODSDB2RQUIYOSTRQVBYOJMHBODVPBYPQ4IYK7MHDOCTZEQVQUVYKNMFBSWYKU4HF6DVLBJUXTLYKVBIPSCYOPANQ4AYOO4FFGDVV7Q5Q56YO2TKCQTYKP4HPWCW4EMFCGCTDBJBQVKYKC4FEWDS5CTKXYRG4BMFPYKHDBJHQ5NAYEBRQVKEDBKLQ4AYOBMFJDLC5XAM56DUHBJNQ4NYOJ4FGKZN3BY7QV2D52MFFBLAISMHMIBCQE4FO6CWTBZ4RJMYKEXEFWDUEBMFC2EYKQBZQ5VONFFNQUZGJ7MHLT2YK44FKGCS4V4FF6CV3B3XQV6YOOBY7CEYKXAATD4YOAX6RSMYOS4FLWDVXBL6Q3YYKKCW6OCUTBLZQVJYKKEWR36YKT4HERVKI5GVQV7KXBL3QVPG7BJXQVEYKUMHJ6DTZ5UHQVFHNK4FFJEYKDS6C3SBN7MHF3KYOHMHBOCSLBKILODRXBLSTGCRLB36ZT3F3B3646CR3BJ7QUFYOPUJQ56D53CUTODW3B3NQ5WYKLAVQUDKAZGDQUAOAP4FMODWQPCVQVHMBEXMTODULB3IHD6HFYMFDY4YK4DRQUQYO64FE6CQJEMHD3TKYSEG4WDWDBZ4T6DXPBJMTJ5YKNQUVGCVIOCEEODSLB2W7ODQQPQEUSPYKHBEFTHYOR4FFOCSQ4E7QUFP4G4FEOCUDB22KLLGM5MHN6DQNGROYQHYO34HB6DSMLUXQVXYKY4HBOCVXBYQMTMYOJ4FKRNYKR4HHY4DJJ4HOLNYKP4HALCYKGMFHJ5YODU7Q43E7BY3Q42YOV4HDLCYKI4HHAXYOFMHOGCVEBR7Q5IYKXGARWCSPBKQHDPYORMHM3SDTBKEQZ4YOVVBQVWYO3MFNOCU7BLDQVIETBLW6OCU55V4JQHYKYUZQUSYKDMHPODVDBIRQ56YOLFSFQGOYNRZQ5NIJ7STQ4MYK3MFFYQYOVCHQV5LHBIRQVANHB3ITCDYKN4FL6DR7BY6L25YO7U5QVXHRP4HKCVYKJSLQ55YK44FEY4O7BLXQUHYOCUFQU3KLB3JQVSBTB3S3TNPLBIKXWCQR5MHCBUYKTQCPIPOBD4HBGDV3B2CZODUE24HJYHYOOMHK6CVYLXNQVZHHBJUDWCR5LMFDZMYONSGWWCVVYMHKWCRHBJPQ5WYO64FIWDSEMMHKC5YOL4HNODRLB3LQ56DVZ4HNQPCQG4FB35MBVMHPOCRZ24HH3RP7BY6CROYKMMHLODS3BYOXQVNHBLLQ4ZYKQVDQU3YKZ4HHLQLPBKLQ43JJ6MFCACOLBZRQ4XYOT4HK6CQMUMHG6DVXBYAPWCRTBYPQ4BORVMFB34YOUMHNCOYK64HBODQMO4FK3ODPBZU6I3YOV4FA6DQ4Q4HNWDVYNHVQVCYK44HI6DXZY4HMZ4YKPD2D2IDRHDOIZYYKYSUYOCRUGMHOTIFEN4FAIROQDMHN3OEBHUK3A2NPBLOE6CUTBKVQ4KYO7QGUCLAEBBIAAKAAAAACAAB7BKDQ4DKIFMHBGDSLB2FQ5EAMAABQ4ZA4AAAEAAAAAAAAAAAAAAAAAAAAAAAAAAABRWK4TUNFTGSY3BORSS42TTN5XFASYFAYAAAAAAAEAACAB6AAAAAEQEAAAAAAA"
-    
-describe('DIVOC QRs', function() {
+const PRODUCTION_PHILIPPINES =
+  'PK\x03\x04\n' +
+  '\x00\x00\x00\b\x00ì\x838SáÒ,Wù\x03\x00\x00Z\x07\x00\x00\x10\x00\x00\x00certificate.json­T[\x8FªH\x10þ+\x84§ÝdPÀË¨/»ÞE\x05T\x84\x83n6\x93\x16\x1AlÅn\x07ZÔ\x99Ì\x7F?Õ:ã8É\x99\x93l²\t\x0FtU}uýª^å¿\x03F9>q¹ñ\x8F¼æ|\x9F5\x8AÅãñX8\x96\n' +
+  ',\x8D\x8BºªÕ\x8AA\x8ACL9AIVÌ5ùáf\x18\x92\x9C\x05\x85$\x0E\x82BÌòB²ýj\x8A\x82\x80PÄ\t£\x02öï\x83ÌÏ{,\x02y8%\x11A«\x04·oöàv\x922\x16Ù\x91÷\x89»S\x03üÓ¹sXmp\x00I¿¾û\x94\'8Í\x18\x05\'$\x84W½ª\x97K¥\x92Zó@\x92âÈø"ÌAHÑNÀúMÏ°$³9\x9F\x0Fº?¤NWr\x8C±×\x04}\x8Ci\x88S°0Q\x82á\x8Dba®\x97/P\x91\x1AJ\b?\x83ÈI\x894Ft\x8BDp\x14\x86)Î2\x91WÆS\x8CyóC ëj½¨K-ÄQ\x82h\x88¤\x19Cá\x83d¢-K\x90ä°\x03_\x7F¼ÀÏ\x17°\x0Eh\x90\x85\x04¤D\x14-Kð\f®á?\x83Îp\fi\x81(ã\x88ãOy\x9B\x1D(O\x85­a\x81tÏ@\x9F´Y(êÙ\x13\x1A\x88¿·7h\\\x96\x1D.\x15\x7F?]ùj\x85h\x80;"\x86¨J×\x14µ®èå¹Vmè\x95F¹T(×\x1F\x97`\x89s\x02Ó\n' +
+  'ÄÀ_¯SùÞñ\x95*¸XÓêZE¯=Ö\x00\x1Fa\x1C®P°uÓä·Ø¿îA\x84Fì?\x01\x02\x9Cràb\x00å\\Hr¯»Ñõ\x93\x90\x82\x86+Ä\x83µ\x98¼aÙ\x93AsfJ\x7FxÝ\x99-µ»ãñ\x9F\x92"\x89\x9E¨Õ\x96¦Wëàä½´ïìÁb\x87è!B\x01?¤\x97ö\x7Fg\x16ÞwüQÑ*sUm\\¾\x82ªª\x97\x8EG\x11,\x05É±ÃQÊ¯¶ª¢é`{¯ua\x85\x92\x9B\'íª\rY\x06Þ5(\x9A\x01=:ð\x02Òê\x90þeUEb¯\x1F;ãÒ-eG*\x03g mrÝ\x83\x9B¶ÙjK&\x0E¡£\x89Ô\x86m\x05èo÷â]5\x86\x1Ei¿bþ\x9D^ÿº\x05·ßÿ}\x19Þ`Ê\x94Ñy\x8Ah\x16áT\x1C*Ðóô <íÅ\x95º»<³\f9$\x06zÀüÄ¹\x94/\x87\n' +
+  '¢\x86¿Þ\x0F1©kW\x83\x0B¥LÌ×,¼Ô\x136²\x94$â\x984vlý\x11krH÷\x97éÈ(Ë\x04]o\x98\x07ys\x14=ÄçázÕ\x0F\x88M\x86S÷ÅÐ,bdÆÎÐ­¶Q]îzY »ð¶ÎÈ\x9F\x12;ÉÈb³P\x8DD«\x17\n' +
+  "­fgD\x97ý\x98ïÖ±³sË1²øYïû\x86§YçÓ¤÷Ò{\x1Eé\\\x99:­¹ßFþÉ¬×5sÍ«¥ã¹\x9FO[M\x80VOÎs·§ÍfQÔ[p'z>ÖèbÔ_+6\x8FúJÛò\\Ý¦n\x8B¾\fÔ­:¬{³¸\x16g6\x99ûãa¹õ8i²¼¬m\x96¶öDéËÊ\x8F,ótÚF±9U\x9C\x11Ë}ËöWð£tNÌ\x1BGhµØ¥?~\x9C#õ\x890¯Ü\x0ECw3;Øá4#Î°¾ì\x1C|Û\x18ùIm6e\x15>ò7jÿyé\x9EÒÜÓùá\x89®ìl°W\rÒM°î¦å\x1Aiw\x17Ä\x9FºÊ¬]Ý3#ï\x06ÓÖvj[\x9D\x0E\x8E&«£Âúií8:\x9C\x9Dm4ÈóýsIéY}T.\x8Fø¹2P\x96^­×\\£îV¯V¦çR/ª\x9BûñÐxJÌ#Pè'PK\x01\x02\x14\x00\n" +
+  "\x00\x00\x00\b\x00ì\x838SáÒ,Wù\x03\x00\x00Z\x07\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00certificate.jsonPK\x05\x06\x00\x00\x00\x00\x01\x00\x01\x00>\x00\x00\x00'\x04\x00\x00\x00\x00"
+
+describe('DIVOC QRs', function () {
   it('should Verify w/ Production Key', async () => {
-    const result = await verifyQRCode(base32url.decodeAsString(PRODUCTION_PAYLOAD_B32URL));
-
-    expect(JSON.parse(result)).to.eql(PRODUCTION_PAYLOAD);
-  });
-});
+    const result = await verifyQRCode(PRODUCTION_PHILIPPINES)
+    expect(result).to.eql(PRODUCTION_PAYLOAD)
+  })
+})
--- test/vds.spec.js
+++ test/vds.spec.js
@@ -1,51 +1,51 @@
 const {verifyQRCode} = require('../lib/index');
 const expect = require('chai').expect; 
 
-const SIGNED_TEST_PAYLOAD = '{"data":{"hdr":{"t":"icao.vacc","v":1,"is":"UTO"},"msg":{"uvci":"U32870","pid":{"n":"Smith Bill","dob":"1990-01-02","sex":"M","i":"A1234567Z","ai":"L4567890Z"},"ve":[{"des":"XM68M6","nam":"Comirnaty","dis":"RA01.0","vd":[{"dvc":"2021-03-03","seq":1,"ctr":"UTO","adm":"RIVM","lot":"VC35679","dvn":"2021-03-24"},{"dvc":"2021-03-24","seq":2,"ctr":"UTO","adm":"RIVM","lot":"VC87540"}]}]}},"sig":{"alg":"ES256","sigvl":"MEQCIDtAxcDjBsNT2I0ry6JDj+sAVAsEREEgJHm5aN1KUzNcAiAUyRiGuA6wDviFP3VprHHqFs6x+JGd/5u15R8snKzdjQ==","cer":"MIIBYDCCAQYCEQCAG8uscdLb0ppaneNN5sB7MAoGCCqGSM49BAMCMDIxIzAhBgNVBAMMGk5hdGlvbmFsIENTQ0Egb2YgRnJpZXNsYW5kMQswCQYDVQQGEwJGUjAeFw0yMTA0MjcyMDQ3MDVaFw0yNjAzMTIyMDQ3MDVaMDYxJzAlBgNVBAMMHkRTQyBudW1iZXIgd29ya2VyIG9mIEZyaWVzbGFuZDELMAkGA1UEBhMCRlIwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARkJeqyO85dyR+UrQ5Ey8EdgLyf9NtsCrwORAj6T68/elL19aoISQDbzaNYJjdD77XdHtd+nFGTQVpB88wPTwgbMAoGCCqGSM49BAMCA0gAMEUCIQDvDacGFQO3tuATpoqf40CBv09nfglL3wh5wBwA1uA7lAIgZ4sOK2iaaTsFNqENAF7zi+d862ePRQ9Lwymr7XfwVm0="}}';
-    
-const TEST_PAYLOAD = {
-    "data": {
-        "hdr": {
-            "t": "icao.vacc",
-            "v": 1,
-            "is": "UTO"
-        },
-        "msg": {
-            "uvci": "U32870",
-            "pid": {
-                "n": "Smith Bill",
-                "dob": "1990-01-02",
-                "sex": "M",
-                "i": "A1234567Z",
-                "ai": "L4567890Z"
-            },
-            "ve": [{
-                "des": "XM68M6",
-                "nam": "Comirnaty",
-                "dis": "RA01.0",
-                "vd": [{
-                    "dvc": "2021-03-03",
-                    "seq": 1,
-                    "ctr": "UTO",
-                    "adm": "RIVM",
-                    "lot": "VC35679",
-                    "dvn": "2021-03-24"
-                }, {
-                    "dvc": "2021-03-24",
-                    "seq": 2,
-                    "ctr": "UTO",
-                    "adm": "RIVM",
-                    "lot": "VC87540"
-                }]
-            }]
-        }
-    }
-};
-
 describe('ICAOs VDS', function() {
-  it('should Verify test payload', async () => {
-    const result = await verifyQRCode(SIGNED_TEST_PAYLOAD);
 
-    expect(result).to.eql(TEST_PAYLOAD);
+  it("should Verify the json from AUS", async () => {
+    // Signed by the original AUS source.  They encoded the JSON as a String
+    const EXPECTED = {
+      data: {
+        hdr: { is: "AUS", t: "icao.vacc", v: 1 },
+        msg: {
+          pid: {
+            dob: "1961-05-15",
+            i: "PA0941262",
+            n: "CITIZEN  JANE SUE",
+            sex: "F",
+          },
+          uvci: "VB0009990012",
+          ve: [
+            {
+              des: "XM68M6",
+              dis: "RA01.0",
+              nam: "AstraZeneca Vaxzevria",
+              vd: [
+                {
+                  adm: "General Practitioner",
+                  ctr: "AUS",
+                  dvc: "2021-09-15",
+                  lot: "300157P",
+                  seq: 1,
+                },
+              ],
+            },
+          ],
+        },
+      },
+      sig: {
+        alg: "ES256",
+        cer: "MIIDhDCCAWygAwIBAgICGK0wDQYJKoZIhvcNAQELBQAwZTELMAkGA1UEBhMCQVUxDDAKBgNVBAoMA0dPVjENMAsGA1UECwwEREZBVDEMMAoGA1UECwwDQVBPMSswKQYDVQQDDCJQYXNzcG9ydCBDb3VudHJ5IFNpZ25pbmcgQXV0aG9yaXR5MB4XDTIxMDgzMTE0MDAwMFoXDTMxMDkzMDEzNTk1OVowHDELMAkGA1UEBhMCQVUxDTALBgNVBAMTBERGQVQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARSVpOyHuLjm01TB1iLBr3SrUp2GkQlM-mPqubbW3mjs0DTeRKrfVTSkkZNgOGj_DB_fo3p8qGy8UVgT4DQRVhIo1IwUDAWBgdngQgBAQYCBAswCQIBADEEEwJOVjAVBgNVHSUBAf8ECzAJBgdngQgBAQ4CMB8GA1UdIwQYMBaAFDYXwef1Z5VxLjd1cI5VgzGG6TgOMA0GCSqGSIb3DQEBCwUAA4ICAQCh_Qc5i6-vewGqinR9EdUpsl0P4jqg0pdx7hyOtPgYOwbTOegJyZOjyWZyuLlxGYuvCHqbrnATMedoIoUJzt8GxHA-4v5TUN2yEbRFXev8ur_0Y3uF4WXFr93Zl0LV78PBNZwXKfZEC6oTN_eVgtR37GdnYsWno0SuhR4fJo8JC_blivas8BJt78Hg8VhvWSK3uT0T58eYQjQhbsXV-BxJ2kSspdvkUF6-arLHh6DVS3ATPAGIm6fEvF4AxnLq5OSHOC3zZR0SR9XntYxEwjo_bW8O0Se8qa5mIBpXmvlwh0Ij6sqVwEskvkM30GmQGfZh5VjFujN2AZnwpjOjK0R-JvR3u6jsBJqVMgm75HgezOzayNiaqzhitrgg5KpO3gK_j3C-Doj5iPAm7I_63GyjUi8ZnqVUZ37UxM19uX2SvhTTQ70nL-zHNfHOyBXJgzMi4Zkor2uagHPz-W1XvNVwGEfFAu-nEyIOKBndHwnvSomL54yBv83X2yAQsoYggU18LNXMHUonTJ_ug7FU0LEX3qA1TeARJ4WBFNjysrBXQepVLowcbtvrhLFjocHjmCp3z17xUoKGI6daajCbvedXgeeSWSD5CuMAXpdN3Yml7VdW7PCK4DD0E_raw6d_wKNGSYAh0TBpNLxnunquai-gFIjgf4iRoys5F35KwmvpZw==",
+        sigvl:
+          "G4-yhmStxY1MML0fLf7LG6OmJXtP6uo5v_fonZ-wiP1N0oSTp9BD8ZqqwHB6uEFukSrsgqBThmOr7aD0_jHY3g==",
+      },
+    };
+
+    const signed =
+      '{"data":{"hdr":{"is":"AUS","t":"icao.vacc","v":1},"msg":{"pid":{"dob":"1961-05-15","i":"PA0941262","n":"CITIZEN  JANE SUE","sex":"F"},"uvci":"VB0009990012","ve":[{"des":"XM68M6","dis":"RA01.0","nam":"AstraZeneca Vaxzevria","vd":[{"adm":"General Practitioner","ctr":"AUS","dvc":"2021-09-15","lot":"300157P","seq":1}]}]}},"sig":{"alg":"ES256","cer":"MIIDhDCCAWygAwIBAgICGK0wDQYJKoZIhvcNAQELBQAwZTELMAkGA1UEBhMCQVUxDDAKBgNVBAoMA0dPVjENMAsGA1UECwwEREZBVDEMMAoGA1UECwwDQVBPMSswKQYDVQQDDCJQYXNzcG9ydCBDb3VudHJ5IFNpZ25pbmcgQXV0aG9yaXR5MB4XDTIxMDgzMTE0MDAwMFoXDTMxMDkzMDEzNTk1OVowHDELMAkGA1UEBhMCQVUxDTALBgNVBAMTBERGQVQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARSVpOyHuLjm01TB1iLBr3SrUp2GkQlM-mPqubbW3mjs0DTeRKrfVTSkkZNgOGj_DB_fo3p8qGy8UVgT4DQRVhIo1IwUDAWBgdngQgBAQYCBAswCQIBADEEEwJOVjAVBgNVHSUBAf8ECzAJBgdngQgBAQ4CMB8GA1UdIwQYMBaAFDYXwef1Z5VxLjd1cI5VgzGG6TgOMA0GCSqGSIb3DQEBCwUAA4ICAQCh_Qc5i6-vewGqinR9EdUpsl0P4jqg0pdx7hyOtPgYOwbTOegJyZOjyWZyuLlxGYuvCHqbrnATMedoIoUJzt8GxHA-4v5TUN2yEbRFXev8ur_0Y3uF4WXFr93Zl0LV78PBNZwXKfZEC6oTN_eVgtR37GdnYsWno0SuhR4fJo8JC_blivas8BJt78Hg8VhvWSK3uT0T58eYQjQhbsXV-BxJ2kSspdvkUF6-arLHh6DVS3ATPAGIm6fEvF4AxnLq5OSHOC3zZR0SR9XntYxEwjo_bW8O0Se8qa5mIBpXmvlwh0Ij6sqVwEskvkM30GmQGfZh5VjFujN2AZnwpjOjK0R-JvR3u6jsBJqVMgm75HgezOzayNiaqzhitrgg5KpO3gK_j3C-Doj5iPAm7I_63GyjUi8ZnqVUZ37UxM19uX2SvhTTQ70nL-zHNfHOyBXJgzMi4Zkor2uagHPz-W1XvNVwGEfFAu-nEyIOKBndHwnvSomL54yBv83X2yAQsoYggU18LNXMHUonTJ_ug7FU0LEX3qA1TeARJ4WBFNjysrBXQepVLowcbtvrhLFjocHjmCp3z17xUoKGI6daajCbvedXgeeSWSD5CuMAXpdN3Yml7VdW7PCK4DD0E_raw6d_wKNGSYAh0TBpNLxnunquai-gFIjgf4iRoys5F35KwmvpZw==","sigvl":"G4-yhmStxY1MML0fLf7LG6OmJXtP6uo5v_fonZ-wiP1N0oSTp9BD8ZqqwHB6uEFukSrsgqBThmOr7aD0_jHY3g=="}}';
+
+    const result = await verifyQRCode(signed);
+    expect(result).to.eql(EXPECTED);
   });
 });
